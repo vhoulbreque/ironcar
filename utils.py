5eb882f48ef9c5b6313234912f16df42182b7e5a
Houlbreque <vincenthoulbreque@gmail.com>
2017-07-08 16:54:21 -0700

    added FolderExistsError and modified print of ArgumentError

--- utils.py
+++ utils.py
@@ -29,25 +29,30 @@ def load_controls(path):
     if not os.path.isfile(path):
         raise IOError
 
-    lines = [l for l in open(path, 'r')]
-
-    new_lines = []
-    for line in lines:
-        words = line.split('#')
-        nl = words[0].replace('\'', '"')
-        if '\n' not in nl:
-            nl += '\n'
-        new_lines.append(nl)
-
-    commands = json.loads(''.join(new_lines))
-    return commands
+    controls = json.loads(''.join([l for l in open(path, 'r')]))
+    return controls
 
 
 class ArgumentError(Exception):
 
-    def __init__(self):
-        pass
+    def __init__(self, placeholder=None):
+        self.placeholder = placeholder
 
     def __str__(self):
-        print('Error in arguments !')
+        display_error_text = 'ArgumentError '
+        if self.placeholder is not None:
+            display_error_text += self.placeholder
+        print(display_error_text)
+        return self
+
+class FolderExistsError(OSError):
+
+    def __init__(self, placeholder=None):
+        self.placeholder = placeholder
+
+    def __str__(self):
+        display_error_text = 'FolderExistsError '
+        if self.placeholder is not None:
+            display_error_text += self.placeholder
+        print(display_error_text)
         return self
