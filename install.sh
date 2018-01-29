ed4bd808b27c80e005872bbf5fb343693b4ad47d
vinzeebreak <vincenthoulbreque@gmail.com>
2018-01-29 14:07:56 +0100

    Added .gitignore. Added test of python's version to ensure the right version of tensorflow is installed

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "-------------------------------"
 echo "IronCar install by XBrain team"
 echo "-------------------------------"
@@ -8,20 +9,37 @@ sudo apt-get update -y
 sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran -y
 sudo apt-get install libhdf5-dev -y
 
-
 sudo apt-get install npm -y
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
-#git checkout ironcar_node
 
-
+# Python
 sudo pip3 install -r requirements_raspi.txt
-wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
-sudo pip3 install tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+
+var=`python3 -c 'import sys; print(sys.version_info[:])'`  # Get the Python version
+set -- $var
+v=$2  # version number
+v=${v2:0:1}
+
+if [ $v -eq 4 ];
+then
+    echo "Python 3.4";
+		wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+		sudo pip3 install tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+elif [ $v -eq 5 ]
+then
+    echo "Python 3.5";
+		wget https://www.dropbox.com/s/gy4kockdbdyx85j/tensorflow-1.0.1-cp35-cp35m-linux_armv7l.whl?dl=1
+		mv tensorflow-1.0.1-cp35-cp35m-linux_armv7l.whl?dl=1 tensorflow-1.0.1-cp35-cp35m-linux_armv7l.whl
+		sudo pip3 install tensorflow-1.0.1-cp35-cp35m-linux_armv7l.whl
+else
+    echo "There is no good version of Python";
+    python3 --version
+fi
+
+# Npm
 npm install
-
-
 
 
 sudo apt-get install -y python-smbus
@@ -30,54 +48,53 @@ sudo apt-get install -y i2c-tools
 
 if grep -Fxq 'i2c-bcm2708' /etc/modules
 then
-	echo $'modules already configured'
+		echo $'modules already configured'
 else
-	echo 'i2c-bcm2708' | sudo tee -a /etc/modules
+		echo 'i2c-bcm2708' | sudo tee -a /etc/modules
 fi
 
 
 if grep -Fxq 'i2c-dev' /etc/modules
 then
-        echo $'modules already configured'
+		echo $'modules already configured'
 else
-	echo 'i2c-dev' | sudo tee -a /etc/modules
+		echo 'i2c-dev' | sudo tee -a /etc/modules
 fi
 
 file="/etc/modprobe.d/raspi-blacklist.conf"
 
 if [ -f "$file" ]
 then
-	if grep -Fxq 'blacklist spi-bcm2708' /etc/modprobe.d/raspi-blacklist.conf
-	then
-		sudo sed -i 's/blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
-		sudo sed -i 's/blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
-	fi
+		if grep -Fxq 'blacklist spi-bcm2708' /etc/modprobe.d/raspi-blacklist.conf
+		then
+				sudo sed -i 's/blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+				sudo sed -i 's/blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
+		fi
 else
-	echo "no blacklist.conf file to modify, everything alright!"
+		echo "no blacklist.conf file to modify, everything alright!"
 fi
 
 if grep -Fxq 'dtparam=i2c1=on' /boot/config.txt
 then
-	echo $'config file already configured'
+		echo $'config file already configured'
 else
-	echo 'dtparam=i2c1=on' | sudo tee -a /boot/config.txt
-
+		echo 'dtparam=i2c1=on' | sudo tee -a /boot/config.txt
 fi
 
 if grep -Fxq 'dtparam=i2c_arm=on' /boot/config.txt
 then
-	echo $'config file already configured'
+		echo $'config file already configured'
 else
-	echo 'dtparam=i2c_arm=on' | sudo tee -a /boot/config.txt
+		echo 'dtparam=i2c_arm=on' | sudo tee -a /boot/config.txt
 fi
 
 if grep -Fxq 'dtparam=i2c1=off' /boot/config.txt
 then
-	sudo sed -i 's/dtparam=i2c1=off/#dtparam=i2c1=off/' /boot/config.txt
+		sudo sed -i 's/dtparam=i2c1=off/#dtparam=i2c1=off/' /boot/config.txt
 fi
 if grep -Fxq 'dtparam=i2c_arm=off' /boot/config.txt
 then
-	sudo sed -i 's/dtparam=i2c_arm=off/#dtparam=i2c_arm=off/' /boot/config.txt
+		sudo sed -i 's/dtparam=i2c_arm=off/#dtparam=i2c_arm=off/' /boot/config.txt
 fi
 
 read -p "Would you like to enable the picamera (y/n)? " CAMCONT
@@ -106,7 +123,6 @@ if [ "$CONT" = "y" ]; then
 else
 	echo "SWAP size was not changed"
 fi
-
 
 
 read -p "We need a reboot, do you want to reboot now (y/n)?" CONT
