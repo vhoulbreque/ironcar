54aa2a540e1c141c6ccd1166cda986d960e38266
Hugoo <hmasclet@gmail.com>
2018-02-19 13:37:06 +0100

    tweak streaming arrow

--- public/js/my_stream.js
+++ public/js/my_stream.js
@@ -6,9 +6,15 @@ $('.stop').hide();
 socket.on('liveStream', function(url_string) {
     $('#stream_image').attr('xlink:href', url_string);
     
-    //TODO find acc and angle in image name
-    var steering = 0;
-    $('#dirline').attr('x2', steering);
+    // TODO find acc and angle in image name
+    var param_arr = url_string.split('_');
+    var steering = parseInt(param_arr[param_arr.length-1].split('.')[0]);
+    console.log(steering);
+
+    // TODO verify if correct. Here we assert many things
+    var steer_to_arrow = ['35','80','125','150','175'];
+
+    $('#dirline').attr('x2', steer_to_arrow[steering]);
     $('.start').hide();
     $('.stop').show();
 });
