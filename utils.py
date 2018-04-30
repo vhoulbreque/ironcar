ce262b7e9d5243e17c905b58ce75b50ec604896b
Vincent Houlbrèque <vincent.houlbreque@supelec.fr>
2018-04-30 23:10:24 +0200

    added comments and cleaned-up code (#38)

--- utils.py
+++ utils.py
@@ -1,8 +1,23 @@
 class ConfigException(Exception):
+    """Handles the Exceptions concerning the Configuration file such as :
+        - absence of the file
+        - absence of one of the necessary fields
+    """
 
     def __init__(self, error_message=None):
         if not error_message:
             error_message = 'The config file is missing some necessary fields'
+
+    def __repr__(self):
+        return error_message
+
+
+class CameraException(Exception):
+    """Handles the Exceptions concerning the PiCamera"""
+
+    def __init__(self, error_message=None):
+        if not error_message:
+            error_message = 'The camera cannot be loaded'
 
     def __repr__(self):
         return error_message
